8484a6e595979110d8cbdb9bb1f141772da55359
marinimarco000 <marini.marco@franchettisalviani.net>
2025-11-04 10:54:20 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace contrario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numrandom = new int[7];
+            Random rnd = new Random();
+
+            //Creazione
+            for( int i = 0 ;i < numrandom.Length;i++)
+            { 
+                numrandom[i] = rnd.Next(0, 7);
+               
+            }
+
+            //Stampa
+            for (int i = 0; i < numrandom.Length; i++)
+            {
+                Console.Write("[" + numrandom[i] + "]");
+
+            }
+
+            Console.WriteLine();
+
+            //Stampa contrario
+            for (int i = numrandom.Length - 1; i >= 0; i--)
+            {
+                Console.Write("["+numrandom[i]+"]");
+            }
+        }
+    }
+}
